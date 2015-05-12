037173f36406f9c301d3fcc8883784a62a7932f6
christopherhuii <christopher.kam.hui@gmail.com>
2015-05-11 18:41:21 -0700

    hacky way to query 0 videos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @search_query = Youtube.new(params[:search] ||= " ")
+    @search_query = Youtube.new(params[:search] ||= "wefawefwafwaefwf") # used that ridiculous string to query 0 videos.
+                                                                        # figure out better way later
   end
 end
