4850108391d56ab155334f31ed966af2972496d6
christopherhuii <christopher.kam.hui@gmail.com>
2015-05-12 17:04:48 -0700

    running code is the same. new ajax method is commented. not fully fleshed out yet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,3 +37,28 @@ $(function(){
 function doneTyping(){
   $('input[name=commit]').click();
 };
+
+
+// USING AJAX
+
+// $(function(){
+//   $('input#search').bind('keyup', function(e){
+//     e.preventDefault();
+//     var convertedQuery = $(this).val().split(' ').join('_');
+
+//     $.ajax({
+//       url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + convertedQuery + "&type=video&key=REMOVED_API_KEYYYYYYY=snippet,contentDetails,statistics,status",
+//       dataType: 'jsonp'
+//     }).done(function(serverData){
+
+//       var videoCount = serverData['items'].length;
+
+//       for (var i = 0; i < videoCount; i++){
+//         $('#videos').append('<li> Title: ' + serverData['items'][i]['snippet']['title'] + '</li>');
+//       };
+//       // console.log(serverData);
+//     }).fail(function(){
+//       console.log("fail..");
+//     });
+//   });
+// })
